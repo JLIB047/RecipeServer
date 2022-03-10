9654ab51978fe1867eecf8b83dd4d0b56c57ff33
Jason Libertelli <jason.libertelli@gmail.com>
2022-03-09 22:31:29 -0500

    deployed16

--- server.js
+++ server.js
@@ -1,29 +1,47 @@
-const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
+// const express = require('express');
+// const { ApolloServer } = require('apollo-server-express');
+// const path = require('path');
 
+// const { typeDefs, resolvers } = require('./schemas');
+// const { authMiddleware } = require('./utils/auth');
+// const db = require('./config/connection');
+
+// const PORT = process.env.PORT || 3001;
+// const app = express();
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware
+// });
+
+// server.applyMiddleware({ app });
+
+// app.use(express.urlencoded({ extended: false }));
+// app.use(express.json());
+
+
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
+
+import {MongoClient, Db, Collection} from 'mongodb'; 
+import ApolloServer from 'apollo-server';
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
-const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+new MongoClient(process.env.MONGODB_URI);
+
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
+    typeDefs,
+    resolvers, 
+    context: authMiddleware,
+    introspection: true,
+    playground: true
 });
 
-server.applyMiddleware({ app });
+server.listen(process.env.PORT);
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
 
