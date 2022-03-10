efcb4b671c3a5b47036aef44accce7501357b7e6
Jason Libertelli <jason.libertelli@gmail.com>
2022-03-09 22:02:07 -0500

    deployed13

--- server.js
+++ server.js
@@ -1,47 +1,29 @@
-// server.js file
-
 const express = require('express');
-const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
+const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
-const mongoose = require('mongoose');
-
-const db = async () => await mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-//   useCreateIndex: true,
-//   useFindAndModify: false,
-});
-
+const { authMiddleware } = require('./utils/auth');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    context: authMiddleware
+  typeDefs,
+  resolvers,
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
- //if in production, serve client/build as static assets
- //if (process.env.NODE_ENV === 'production') {
-   //  app.use(express.static(path.join(__dirname, '../client/build')));
- //};
 
- //app.get('*', (req, res) => {
-   // res.sendFile(path.join(__dirname, '../client/build/index.html'));
- //});
-
- db('open',  () => {
-     app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-//     console.log(`GraphQL server readt at http://localhost:${PORT}${server.graphqlPath}`);
- });
-
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
+});
 
